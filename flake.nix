8eac162b37721b552358ce7a541986406ff9fb07
Naexon <naexoncode@pm.me>
2024-11-05 11:57:05 +0100

    cleanup

--- flake.nix
+++ flake.nix
@@ -34,29 +34,17 @@
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             cargo
-            openssl
-            openssl.dev
-            pkg-config
-            perl
-            cairo-bin.stable.cairo
+            # cairo-bin.stable.cairo
             cairo-bin.stable.scarb
           ];
-
-          # Set OpenSSL-related environment variables
-          OPENSSL_DIR = "${pkgs.openssl.dev}";
-          OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
-          OPENSSL_LIB_DIR = "${pkgs.openssl.dev}/lib";
-          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-
-          RUSTFLAGS = "--cfg openssl_dir=/nix/store/s3vv0916j6z5zfs0b1bcfinx1kb6cbhn-openssl-3.3.2-bin/bin/openssl";
         };
 
         packages = {
           default = pkgs.cairo-bin.stable.scarb;
-          cairo = pkgs.cairo-bin.stable.cairo;
-          scarb = pkgs.cairo-bin.stable.scarb;
-          cairo-beta = pkgs.cairo-bin.beta.cairo;
-          scarb-beta = pkgs.cairo-bin.beta.scarb;
+          # cairo = pkgs.cairo-bin.stable.cairo;
+          # scarb = pkgs.cairo-bin.stable.scarb;
+          # cairo-beta = pkgs.cairo-bin.beta.cairo;
+          # scarb-beta = pkgs.cairo-bin.beta.scarb;
         };
       });
 }
